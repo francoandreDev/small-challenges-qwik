51e339c27c410f7a59acc5ad133d0c3dec282d04
francoandre.dev <francoandre.dev@gmail.com>
2023-12-05 14:41:33 -0500

    feature add images and better logic in styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,8 @@ import { component$ } from "@builder.io/qwik";
 import { CarrouselComponent as Carrousel } from "./Carrousel";
 
 export const App = component$(() => {
-    return <div class="text-neutral-400 bg-gray-900 h-full min-h-screen flex flex-col flex-wrap gap-20 items-center py-10 font-bold text-5xl overflow-x-hidden">
+    return <div class="text-neutral-400 bg-gray-900 h-full min-h-screen flex flex-col flex-wrap gap-20 items-center py-10 font-bold text-5xl overflow-x-hidden relative">
         <h1>My App</h1>
-        <Carrousel />
+        <Carrousel totalCards={8} />
     </div>
 })
--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -1,8 +1,7 @@
 import { component$, useSignal, $ } from "@builder.io/qwik";
 import { CardComponent as Card } from "./Card";
 
-export const CarrouselComponent = component$(() => {
-    const totalCards = 5
+export const CarrouselComponent = component$(({ totalCards }: { totalCards: number }) => {
     const currentPos = useSignal<number>(0);
 
     const previousCard = $(() => {
@@ -17,13 +16,13 @@ export const CarrouselComponent = component$(() => {
 
     return <div class="carrousel-component flex flex-col gap-11 w-96">
         <h2 class="text-center">Carrousel</h2>
-        <div class="cards flex flex-nowrap gap-40 mt-7">
+        <div class="cards flex flex-nowrap gap-40 mt-7 z-10">
             {new Array(totalCards).fill(0).map((_e, index) => {
                 const pos = (currentPos.value + index) % totalCards
-                return <Card position={pos} />
+                return <Card position={pos} key={index} />
             })}
         </div>
-        <div class="flex flex-wrap gap-10 justify-center items-center">
+        <div class="flex flex-nowrap gap-56 justify-center items-center w-96 relative z-10">
             <button onClick$={previousCard}>Before</button>
             <button onClick$={nextCard}>Next</button>
         </div>
