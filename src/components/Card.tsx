f7a6d34e350fba54335f9411180f7c07d442988f
francoandre.dev <francoandre.dev@gmail.com>
2023-12-05 15:04:01 -0500

    fix bug first image was not change at all

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,10 +2,8 @@ import { component$ } from "@builder.io/qwik";
 import styles from "../css-modules/styles.module.css";
 
 export const CardComponent = component$(({ position }: { position: number }) => {
-    return <div
-        class={`${styles["card-component"]} ${position === 0 ? styles["max-size"] : styles["default-size"]}
-    ${styles[`style-card-${position % 5}`]}
-     w-80 border-2 flex justify-center items-center gap-8 py-10 px-5 rounded-lg min-w-[10rem]`}
-    >
+    return <div class={`${styles["card-component"]}
+     ${styles[`style-card-${position % 5}`]}
+     w-80 border-2 flex justify-center items-center gap-8 py-10 px-5 rounded-lg min-w-[10rem]`}>
     </div>
 });
